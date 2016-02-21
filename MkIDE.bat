6076e4943b20e790c04434e69ae6bfe2e3a1290f
jgharston <jgh@mdfs.net>
2016-02-21 00:49:16 +0000

    Added options KEEP_REDUNDANCY and LARGE_DISK
    Added options:
     KEEP_REDUNDANCY - remove redundant code that patch options leave in
     LARGE_DISK - preliminary work to identify where top bits of sector number are lost
    Corrected error numbers in MMC.asm

---
+++ MkIDE.bat
@@ -0,0 +1,3 @@
+cd %0\..
+C:\Apps\Programming\BeebAsm\BeebAsm.exe -i adfsIDE.asm -do ADFSIDE.ssd -o ADFSIDE -v > ADFSIDE.lst
+if ERRORLEVEL 1 pause
---
+++ MkMMC.bat
@@ -0,0 +1,3 @@
+cd %0\..
+C:\Apps\Programming\BeebAsm\BeebAsm.exe -i adfsMMC.asm -do ADFSMMC.ssd -o ADFSMMC -v > ADFSMMC.lst
+if ERRORLEVEL 1 pause
